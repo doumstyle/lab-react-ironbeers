0ba8dbff594e43188aea69336875b83fad6114f3
Mamadou AMADOU <doumstyle@gmail.com>
2022-01-30 09:27:31 +0100

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home';
 import AllBeers from './components/AllBeers';
 import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer';
+import BeerDetails from './components/BeerDetails';
 
 function App() {
 
@@ -12,10 +13,11 @@ function App() {
 
 
   return (
-    <div className="container">
+    <div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/beers' element={<AllBeers />} />
+        <Route path='/beers/:id' element={<BeerDetails />} />
         <Route path='/random-beer' element={<RandomBeer />} />
         <Route path='/new-beer' element={<NewBeer />} />
       </Routes>
